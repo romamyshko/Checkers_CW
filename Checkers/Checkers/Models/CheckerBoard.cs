f0e31d2d7766a96c26282c8d43ed5714c72b7b26
Roman Myshko <romamyshko12@gmail.com>
2022-06-23 10:55:45 +0300

    Add new model type 'Move'.

--- Checkers/Checkers/Models/CheckerBoard.cs
+++ Checkers/Checkers/Models/CheckerBoard.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace Checkers.Models
 {
-    internal class CheckerBoard
+    public class CheckerBoard
     {
+        public int[,] Board { get; } = new int[8, 8];
+
+        public CheckerBoard()
+        {
+            InitialiseBoard();
+        }
+
+        public bool SetState(int r, int c, int state)
+        {
+            if (state is > 4 or < -1)
+                return false;
+
+            Board[r, c] = state;
+            return true;
+        }
+
+        public int GetState(int r, int c)
+        {
+            if (r is > 7 or < 0 || c is > 7 or < 0)
+                return -1;
+
+            return Board[r, c];
+        }
+
+        private void InitialiseBoard()
+        {
+            for (var r = 0; r < 8; r++)
+            {
+                for (var c = 0; c < 8; c++)
+                {
+                    Board[r, c] = -1;
+                }
+            }
+        }
     }
 }
---
+++ Checkers/Checkers/Models/Move.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers.Models
+{
+    public class Move
+    {
+        public Piece Piece1 { get; }
+        public Piece Piece2 { get; }
+
+        public Move(Piece p1, Piece p2)
+        {
+            Piece1 = p1;
+            Piece2 = p2;
+        }
+
+        public bool IsAdjacent(string color)
+        {
+            switch (color)
+            {
+                case "Black":
+                    if (Piece1.Row - 1 == Piece2.Row && Piece1.Column - 1 == Piece2.Column)
+                        return true;
+                    if (Piece1.Row - 1 == Piece2.Row && Piece1.Column + 1 == Piece2.Column)
+                        return true;
+                    break;
+                case "White":
+                    if (Piece1.Row + 1 == Piece2.Row && Piece1.Column - 1 == Piece2.Column)
+                        return true;
+                    if (Piece1.Row + 1 == Piece2.Row && Piece1.Column + 1 == Piece2.Column)
+                        return true;
+                    break;
+                case "King":
+                    if (Piece1.Row - 1 == Piece2.Row && Piece1.Column - 1 == Piece2.Column)
+                        return true;
+                    if (Piece1.Row - 1 == Piece2.Row && Piece1.Column + 1 == Piece2.Column)
+                        return true;
+                    if (Piece1.Row + 1 == Piece2.Row && Piece1.Column - 1 == Piece2.Column)
+                        return true;
+                    if (Piece1.Row + 1 == Piece2.Row && Piece1.Column + 1 == Piece2.Column)
+                        return true;
+                    break;
+            }
+
+            return false;
+        }
+
+        public Piece CheckJump(string color)
+        {
+            switch (color)
+            {
+                case "Black":
+                    if (Piece1.Row - 2 == Piece2.Row && Piece1.Column - 2 == Piece2.Column)
+                        return new Piece(Piece1.Row - 1, Piece1.Column - 1);
+                    if (Piece1.Row - 2 == Piece2.Row && Piece1.Column + 2 == Piece2.Column)
+                        return new Piece(Piece1.Row - 1, Piece1.Column + 1);
+                    break;
+                case "White":
+                    if (Piece1.Row + 2 == Piece2.Row && Piece1.Column - 2 == Piece2.Column)
+                        return new Piece(Piece1.Row + 1, Piece1.Column - 1);
+                    if (Piece1.Row + 2 == Piece2.Row && Piece1.Column + 2 == Piece2.Column)
+                        return new Piece(Piece1.Row + 1, Piece1.Column + 1);
+                    break;
+                case "King":
+                    if (Piece1.Row - 2 == Piece2.Row && Piece1.Column - 2 == Piece2.Column)
+                        return new Piece(Piece1.Row - 1, Piece1.Column - 1);
+                    if (Piece1.Row - 2 == Piece2.Row && Piece1.Column + 2 == Piece2.Column)
+                        return new Piece(Piece1.Row - 1, Piece1.Column + 1);
+                    if (Piece1.Row + 2 == Piece2.Row && Piece1.Column - 2 == Piece2.Column)
+                        return new Piece(Piece1.Row + 1, Piece1.Column - 1);
+                    if (Piece1.Row + 2 == Piece2.Row && Piece1.Column + 2 == Piece2.Column)
+                        return new Piece(Piece1.Row + 1, Piece1.Column + 1);
+                    break;
+            } 
+
+            return null!;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Move m)
+            {
+                return Piece1.Equals(m.Piece1) && Piece2.Equals(m.Piece2);
+            }
+
+            return false;
+        }
+
+        protected bool Equals(Move other)
+        {
+            return Piece1.Equals(other.Piece1) && Piece2.Equals(other.Piece2);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Piece1, Piece2);
+        }
+    }
+}
